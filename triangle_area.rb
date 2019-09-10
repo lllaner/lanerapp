e01665a559d8f9bba420fab6b0c35298ad539d47
Laner <iifunnymomen@gmail.com>
2019-09-10 13:39:55 +0300

    create tiriangle area programm

---
+++ triangle_area.rb
@@ -0,0 +1,6 @@
+puts "Please enter triangle base"
+triangle_base = gets.chomp
+puts "Please enter triangle hight"
+triangle_hight = gets.chomp
+triangle_area = 0.5 * triangle_hight.to_i * triangle_base.to_i
+puts "triangle area is #{triangle_area}"
