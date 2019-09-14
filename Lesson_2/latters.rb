d953d9bb8c153ee6b5ba795910fa1573e6edacc4
Laner <iifunnymomen@gmail.com>
2019-09-14 23:07:54 +0300

    add comments

--- Lesson_2/latters.rb
+++ Lesson_2/latters.rb
@@ -1,6 +1,10 @@
+#Create alphabet
 latters = ("a".."z").to_a
 number = (0..25).to_a
 all_latters = {}
-number.each {|x| all_latters[latters[x]] = x +1 if latters[x][/[aeiou]/]}
-p all_latters
 
+#create hash with vowels
+number.each {|x| all_latters[latters[x]] = x +1 if latters[x][/[aeiou]/]}
+
+#Output vowels latters
+p all_latters
